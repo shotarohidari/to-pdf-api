1db9f144a1b140e98d99ee5725639a3e582a1979
左翔太朗 <zuoxiangtailang@sashoutarous-MacBook-Air.local>
2024-05-08 11:33:55 +0900

    CDKの設定

--- lib/to-pdf-api-stack.ts
+++ lib/to-pdf-api-stack.ts
@@ -1,16 +1,26 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib"
+import { Duration } from "aws-cdk-lib"
+import { FunctionUrlAuthType, Runtime } from "aws-cdk-lib/aws-lambda"
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs"
+import { Construct } from "constructs"
 
 export class ToPdfApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const fn = new NodejsFunction(this, "lambda", {
+      entry: "lib/lambda/index.ts",
+      handler: "handler",
+      runtime: Runtime.NODEJS_20_X,
+      timeout: Duration.minutes(3),
+      memorySize: 512,
+      bundling: {
+        nodeModules: ["@sparticuz/chromium"],
+      },
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ToPdfApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    fn.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    })
   }
 }
